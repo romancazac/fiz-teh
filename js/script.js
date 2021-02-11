639ce314de7d451919c658c51061469e182e74f4
romancazac <70430456+romancazac@users.noreply.github.com>
2021-02-11 18:53:54 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+// const intem = document.querySelectorAll(".language__item");
+// for (let index = 0; index < intem.length; index++) {
+//     const inte = intem[index];
+//     inte.addEventListener("click", function(){
+//       inte.classList.remove('active');
+//       inte.classList.toggle('active');
+        
+        
+//     });
+    
+// }
+//scrollTop
+
+const offset = 500;
+const scrollUp = document.querySelector('.scroll-top ');
+
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+const updateDashoffset = () => {};
+
+// onscroll
+window.addEventListener('scroll', () => {
+  if (getTop() > offset) {
+    scrollUp.classList.add ('active');
+  } else {
+    scrollUp.classList.remove ('active');
+  }
+
+});
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top:0,
+    behavior:'smooth'
+  });
+
+
+});
+
+
 
 $('.section-doc__slider').slick({
   slidesToShow: 3,
@@ -24,27 +64,39 @@ $('.section-doc__slider').slick({
             slidesToScroll: 1
         }
     }
-   
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
 ]
   
 });
 
 const headeBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__nav');
-const headerList = document.querySelector('.header__list');  
+const headerList = document.querySelector('.header__list');
+const body = document.body;
+
+function disableScroll () {
+  body.classList.add('disble-scroll');
+}
+function enableScroll () {
+  body.classList.remove('disble-scroll');
+}
     
       headeBurger.addEventListener("click", function (e) {
       
       
         headeBurger.classList.toggle('active');
         headerMenu.classList.toggle('active');
+        disableScroll ()
+
 });
 headerList.addEventListener("click", function (e) {
       
       
   headeBurger.classList.remove('active');
   headerMenu.classList.remove('active');
-  
+  enableScroll ()
 
 });
 
@@ -79,27 +131,34 @@ $(function() {
    
   })();
 
-const offset = 500;
-const scrollUp = document.querySelector('.scroll-top ');
-
-
-const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
-
-const updateDashoffset = () => {};
-window.addEventListener('scroll', () => {
-  if (getTop() > offset) {
-    scrollUp.classList.add ('active');
-  } else {
-    scrollUp.classList.remove ('active');
-  }
-
-});
-scrollUp.addEventListener('click', () => {
-  window.scrollTo({
-    top:0,
-    behavior:'smooth'
-  });
-
-
-});
+  
  
+  
+  
+  const btns= document.querySelectorAll('.baner__link');
+  const modalOverlay = document.querySelector('.modal-overlay ');
+  const modals = document.querySelectorAll('.modal');
+  
+  btns.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      let path = e.currentTarget.getAttribute('data-path');
+  
+      modals.forEach((el) => {
+        el.classList.remove('modal--visible');
+      });
+  
+      document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+      modalOverlay.classList.add('modal-overlay--visible');
+    });
+  });
+  
+  modalOverlay.addEventListener('click', (e) => {
+    
+  
+    if (e.target == modalOverlay) {
+      modalOverlay.classList.remove('modal-overlay--visible');
+      modals.forEach((el) => {
+        el.classList.remove('modal--visible');
+      });
+    }
+  });
